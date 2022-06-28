631beb9a7963c4ceda268f56292784af9353015f
brad-huey <bradnhuey@gmail.com>
2022-06-28 16:30:50 -0400

    Finsh project - notes included
    Add button to start game, add logging rounds and results. Game is complete. Will commit without notes after

--- javascript.js
+++ javascript.js
@@ -1,36 +1,54 @@
-//Step 12 - Change if statement under playerChoice prompt to notify player of typo with while loop
-//Step 13 - Loop needs to end when player enters correct spelling and game continues, redefine value of input
-//Step 14 - Run check against input again to end loop with true
-//Step 15 - Fix cancel button by adding the null loop into the new typo loop (this appears to be broken still diagnose later)
-//Step 16 - Add new function checkWinner with paramets determined by the players choice and computers choice
-//Step 17 - Use if statement to conclude tie game if chosenByPlayer = chosenByComputer
-//Step 18 - Process all possible win scenarios for chosenByPlayer using if || statements
-//Step 19 - After concluding all outcomes for tie and win, an else statement determines computer wins
-//Step 20 - Create const winner to determine parameter outcome for function checkWinner, input coinsole.log after to test
-//Step 21 Add return input; at the end of playerChoice because it was not receiving player input
-
+//Step 22 - Create for loop to limit the amount of rounds to 5, and it will no longer stop after one round either.
+//Step 23 - To log winners we need to create global const winners with an empty array, and add a function under function playRound
+//to push each winner into the empty array
+//Step 24 - Create a new function called logWins and add console.log(winners) to it
+//Step 25 - Add new logWins() function into function game() so it counts each round
+//Step 26 - Remove console.logs(winners) create the array seen, and use console.logs to print the results
+//Step 27 - Add logRound within playRound function so it logs each round
+//Step 28 - Pass i from function game loop into the round() so it's round(i)
+//Step 29 - Change some grammar to make the results make more sense, added new console.log '---' for readability
+//Step 30 - Remove game(); at end of code, added a button in html file to start game so we can get to console
+//before the game begins -- fixed tie game logging bug due to grammar inconsistancy
+//Step 31 - Add a play new game feature to the button, for future reference (under function game())
 
 
 const choices = ['rock', 'paper', 'scissors'];
+const winners = [];
 
 function game(){
-    playRound();
+    for(let i = 1; i <= 5; i++){
+        playRound(i);
+    }
+    document.querySelector('button').textContent = "Play Again?";
+    logWins();
 }
+//To make game last 5 rounds you could copy function playRound 5x but that is not an approach you can scale easily.
+//Create a for loop. For loop is *for* a set amount until loop completes, *while* is during conditions being met
+//(i is the common variable used for loops) i = 0 means the counter starts at 0 so start it at 1
+// and as long as it is < or = 5
+//the loop will continue. i++ adds 1 to i until it reaches 5.
 
+//Add logWins(); function to add that function into each game round
 
-function playRound() {
+//Add for loop variable (i) into playRound. This is required for playRound to receive the round number so we can log it.
+
+//Added function to play again to the button just for future reference
+
+function playRound(round) {
     const playerSelection = playerChoice();
     const computerSelection = computerChoice();
     const winner = checkWinner(playerSelection, computerSelection)
-    console.log(winner)
-    //Add new const winner to determine chosenByPlayer/Computer outcome with checkWinner
-    //Make parameters playerSelection and computerSelection because --
-    /* playerSelection const = playerChoice function, playerChoice function is used for input and validation to convert const choices
-    string (rock, paper, scissors) into prompt, player prompt result string is converted to number to use in checkWinner function as
-    chosenByPlayer/Computer parameters, chosenByP/C parameters used to determine winner and result of checkWinner function makes
-    const winner = const player/computerSelection. Guarunteed you will be trippd up by 'choice' parameter and const choices.
-    */
-    // Close lose end with console.log
+    winners.push(winner);
+    logRound(playerSelection, computerSelection, winner, round)
+  
+    // move console.log(winner) and place under new function logWin
+    //use new const winners to push winner into empty winners array (pay attention to the plural here)
+    //this is to make a log for who won
+
+    //Add logRound from the new function to be within the playRound function
+
+    //Add round as parameter in playRound function and round into the logRound parameters
+    //playRound now has i from function game loop in it, and this will pass it through to logRound to log it
 }
 
 function playerChoice() {
@@ -43,7 +61,7 @@ function playerChoice() {
     while (check === false) {
        input = prompt('Spelling needs to be correct! Capitalization not required. Type Rock, Paper, or Scissors');
         while (input === null) {
-        input = prompt('Spelling needs to be correct! Capitalization not required. Type Rock, Paper, or Scissors') //while loop copied from above to work in both loops
+        input = prompt('Spelling needs to be correct! Capitalization not required. Type Rock, Paper, or Scissors')
         
         }
         input = input.toLowerCase();
@@ -51,17 +69,6 @@ function playerChoice() {
     }
     return input;
 }
-
-    //let check = validateInput(input)
-        //if (check === true) {
-            //changed to while loop to check for false to change prompt notifying player of typo
-    //Because new prompt will continue to loop, you need to end the loop by redefining input
-    //after redefining you need to re-check validation by running check against validateInput(input) again
-    //When check is no longer false, the game will continue
-    //copy while loop for null (cancel button) into new loop so cancel will work before and after typo
-    //moved comments below block
-
-    //Added return input; outside of loop to enter the players input
 
 
 
@@ -76,38 +83,45 @@ function validateInput(choice){
 
 function checkWinner(chosenByPlayer, chosenByComputer){
     if(chosenByPlayer === chosenByComputer){
-        return 'Tie';
-        //Below, using || we can create cleaner lines than if and else statements seen in notes below
-        //Comparing all outcomes for chosenByPlayer to win and chosenByComputer to lose
-        //Use else statement for computer to win because tie and win for player are already processed
+        return 'Tie game this';
     } else if (
         (chosenByPlayer === 'rock' && chosenByComputer === 'scissors') ||
         (chosenByPlayer === 'paper' && chosenByComputer === 'rock') ||
         (chosenByPlayer === 'scissors' && chosenByComputer === 'paper')
         ) { 
-            return 'Player Win'; 
+            return 'Player Wins'; 
         }   else {
-            return 'Computer Win';
+            return 'Computer Wins';
         }
 }
-//added function checkWinner to determine who wins game with parameters determined by what player and computer chose
-//start with tie, if chosenByPlayer is the same as computer return Tie will print
-//Use else if to go through all outcomes, use && (and, not || or!) to compare player and computer to return winner/loser
-/* This is that outcome:
-function checkWinner(chosenByPlayer, chosenByComputer){
-    if(chosenByPlayer === chosenByComputer){
-        return 'Tie';
-    } else if (chosenByPlayer === 'rock' && chosenByComputer === 'scissors'){
-        return 'Player Win';
-    } else if (chosenByPlayer === 'scissors' && chosenByComputer === 'paper'){
-        return 'Player Win';
-    } else if (chosenByPlayer === 'paper' && chosenByComputer === 'rock'){
-        return 'Player Win';
-    } else {
-        return 'Computer Win';
-    }
+
+function logWins() {
+    //console.log(winners) remove for now
+    let playerWins = winners.filter((item) => item == 'Player Wins').length;
+    let computerWins = winners.filter((item) => item == 'Computer Wins').length;
+    let tieGames = winners.filter((item) => item == 'Tie game this').length;
+    console.log('FINAL RESULTS:');
+    console.log('Player wins:', playerWins);
+    console.log('Computer wins:', computerWins);
+    console.log('Tie Games:', tieGames);
+
+    //This is an array, what it will do is check over each playerWins, computerWins, tieGames
+    //If player doesn't win it will not count it toward playerWins. If playerWins it will count it. etc.
+    //The item is the tally, the 'Player Win' (and others) HAS to be the same as the return under checkWinner
+    //because that's what this is array is looking for to add to the tally
+    //.length will tell us how many times for each
+    //console.log for both 'Results: ' and 'Player Wins:' playerWins (followed by computer and ties)
+    //FIX- 'Time game this' needed to be matched to checkWinner function when I editted for grammar
 }
 
-BUT you can use || (or) to simplyfy as seen above! */
+function logRound(playerChoice, computerChoice, winner, round) {
+    console.log('Round: ', round);
+    console.log('Player chose: ', playerChoice);
+    console.log('Computer chose: ', computerChoice);
+    console.log(winner, 'round');
+    console.log('---------------------------------------------')
 
-game();
+    //Create function logRound with parameters for what the player chose, the computer chose, and who won.
+    //Include round number as well. This will be displayed after the game. This will be added into playRound function.
+    //Add final '---' as a separator for readability in the console
+}
